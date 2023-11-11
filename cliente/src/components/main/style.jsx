9627a19faa7ceedcd81b01daa3752b13a6043814
José Aparecido <juniorsilva240803@gmail.com>
2023-11-11 16:11:40 -0300

    build: added build configuration

--- cliente/src/components/main/style.jsx
+++ cliente/src/components/main/style.jsx
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  width: 100%;
+  max-width: 1900px;
   height: 100vh;
   display: flex;
   justify-content: space-between;
+  align-self: center;
+  gap: 2rem;
 `;
